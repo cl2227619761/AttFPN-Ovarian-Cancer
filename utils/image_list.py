c51d636e4cb10d587fa9bcd01c0a6c071adcc49f
cl2227619761 <stat-caolei@localhost.localdomain>
2021-05-25 21:06:40 +0800

    update

--- utils/image_list.py
+++ utils/image_list.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 # coding=utf-8
-"""
-本脚本的作用是将预处理后的4D图像张量和尺寸列表捆绑在一起
-"""
 import torch
 from torch.jit.annotations import List, Tuple
 
 
 class ImageList(object):
-    """
-    作用:
-        将图像和尺寸放在该结构中，便于调用
-    """
 
     def __init__(self, tensors, image_sizes):
         # type: (Tensor, List[Tuple[int, int]])
