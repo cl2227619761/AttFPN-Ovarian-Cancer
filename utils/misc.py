d0c0e5f769e356aaff0ae6dd1fc4ae0c6a741930
cl2227619761 <stat-caolei@localhost.localdomain>
2021-05-25 21:28:56 +0800

    update

--- utils/misc.py
+++ utils/misc.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
-"""
-该脚本是提供将backbone网络的batchnorm层进行固定的
-"""
 import torch.nn as nn
 import torch
 
 
 class FrozenBatchNorm2d(nn.Module):
-    """
-    作用:固定gamma, beta, rm, rv版的BatchNorm2d
-    参数:
-        n: 输入的张量的通道数
-    """
 
     def __init__(self, n):
         super(FrozenBatchNorm2d, self).__init__()
-        # 将gamma, beta, rm, rv注册为buffer
         self.register_buffer("weight", torch.ones(n))
         self.register_buffer("bias", torch.zeros(n))
         self.register_buffer("running_mean", torch.zeros(n))
@@ -25,9 +16,6 @@ class FrozenBatchNorm2d(nn.Module):
     def _load_from_state_dict(self, state_dict, prefix, local_metadata,
                               strict, missing_keys, unexpected_keys,
                               error_msgs):
-        """
-        作用:删除以num_batch_tracked为后缀的键
-        """
         num_batches_tracked_key = prefix + "num_batches_tracked"
         if num_batches_tracked_key in state_dict:
             del state_dict[num_batches_tracked_key]
