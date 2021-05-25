2e1570bc5e981ff81689201f37e43e512537fc53
cl2227619761 <stat-caolei@localhost.localdomain>
2021-05-25 21:29:58 +0800

    update

--- utils/pooler.py
+++ utils/pooler.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
 # coding=utf-8
-"""
-将所有的roi在每个水平的特征上都进行一次roialign操作，参考论文
-Path Aggregation Network
-"""
 from torchvision.ops import MultiScaleRoIAlign
 from torchvision.ops import roi_align
 
 
 class NewMultiScaleRoIAlign(MultiScaleRoIAlign):
-    """
-    继承基类，对其进行改造，因为基类是参考FPN论文进行编写的，而我们
-    并不需要按照proposal的尺寸将其映射到对应的水平，我们要做的是在
-    每个水平上都进行roialign
-    """
 
     def __init__(self, featmap_names, output_size, sampling_ratio):
         super(NewMultiScaleRoIAlign, self).__init__(
@@ -21,14 +12,6 @@ class NewMultiScaleRoIAlign(MultiScaleRoIAlign):
         )
 
     def forward(self, x, boxes, image_shapes):
-        """
-        修改基类的forward方法
-        参数:
-            x: Dict[str, Tensor]
-            boxes: List[Tensor]
-            image_shapes: List[Tuple[int, int]]
-        其中x是所有水平的特征构成的字典，boxes是proposals
-        """
         x_filtered = []
         for k, v in x.items():
             if k in self.featmap_names:
