0623dc23d8351ba192dc8ec4c72f0e41cc25f3b5
cl2227619761 <stat-caolei@localhost.localdomain>
2021-05-25 20:28:47 +0800

    update

--- utils/box_ops.py
+++ utils/box_ops.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # coding=utf-8
-"""
-该脚本的作用是提供一些关于框的运算操作，比如nms等运算
-"""
 import torch
 import torchvision
 from torchvision.ops import nms
@@ -12,17 +9,6 @@ from torch import Tensor
 
 def batched_nms(boxes, scores, idxs, iou_threshold):
     # type: (Tensor, Tensor, Tensor, float)
-    """
-    作用:
-        在每个水平或者每个类别中独立地做NMS
-    参数:
-        boxes: 一张图像上的预测框张量
-        scores: 这些框对应的logit值张量
-        idxs: 这些框对应的类别或者水平张量
-        iou_threshold: NMS时设置的iou阈值
-    返回:
-        keep: 要保留的框的索引张量
-    """
     if boxes.numel() == 0:
         return torch.empty((0,), dtype=torch.int64, device=boxes.device)
 
@@ -35,15 +21,6 @@ def batched_nms(boxes, scores, idxs, iou_threshold):
 
 def clip_boxes_to_image(boxes, size):
     # type: (Tensor, Tuple[int, int])
-    """
-    作用:
-        保证一张图像上的候选框都在图像的内部，没有超出边界
-    参数:
-        boxes: 一张图像的预测框坐标张量
-        size: 一张图像的尺寸
-    返回:
-        经过限制处理以后的候选框坐标张量
-    """
     dim = boxes.dim()
     boxes_x = boxes[..., 0::2]
     boxes_y = boxes[..., 1::2]
@@ -65,15 +42,6 @@ def clip_boxes_to_image(boxes, size):
 
 def remove_small_boxes(boxes, min_size):
     # type: (Tensor, float)
-    """
-    作用:
-        去除尺寸小于指定最小尺寸的预测框
-    参数:
-        boxes: 预测框的坐标张量
-        min_size: 指定的最小尺寸
-    返回:
-        要保留的预测框的索引
-    """
     ws, hs = boxes[:, 2] - boxes[:, 0], boxes[:, 3] - boxes[:, 1]
     keep = (ws >= min_size) & (hs >= min_size)
     keep = keep.nonzero().squeeze(1)
@@ -81,18 +49,10 @@ def remove_small_boxes(boxes, min_size):
 
 
 def box_area(boxes):
-    """
-    作用:
-        计算一张图片上所有框的面积
-    """
     return (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
 
 
 def box_iou(boxes1, boxes2):
-    """
-    作用:
-        计算boxes1中的每个框和boxes2中的每个框的iou
-    """
     area1 = box_area(boxes1)
     area2 = box_area(boxes2)
 
